38d608bd2951a3ebfe89a38be886d2ccae1c42a5
Mani Kanta <mk2004t@gmail.com>
2026-07-13 21:08:45 +0530

    Day 10 of 100

---
+++ day10/docstrings.py
@@ -0,0 +1,5 @@
+def full_name(f_name,l_name):
+  """Takes you firstname and lastname convert into to title case and print the fullname by concatenate those two individual names."""
+  return f"{f_name.title()} {l_name.title()}"
+
+full_name(f_name="TMK",l_name="Rishi")
