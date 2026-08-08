3f1c614ead775983e5e79425503fb109b81fa686
Mani Kanta <mk2004t@gmail.com>
2026-08-08 19:40:57 +0530

    little tweak to Day22

--- day22/ball.py
+++ day22/ball.py
@@ -8,14 +8,25 @@ class Ball(Turtle):
     self.penup()
     self.x_add = 10
     self.y_add = 10
+    self.move_speed = 0.1
 
   def move(self):
     new_x = self.xcor() + self.x_add
     new_y = self.ycor() + self.y_add
     self.goto(x=new_x,y=new_y)
 
-  def bounce(self):
+  def bounce_y(self):
     self.y_add*= -1
+    self.move_speed *= 0.9
+
+  def bounce_x(self):
+    self.x_add*= -1
+    self.move_speed *=0.9
+
+  def reset_position(self):
+    self.move_speed = 0.1
+    self.goto(x=0,y =0)
+    self.bounce_x()
     
 
   
--- day22/the_pong.py
+++ day22/the_pong.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 #Setting up screen
 screen = Screen()
@@ -18,26 +19,35 @@ screen.onkey(r_paddle.Up,"Up")
 screen.onkey(r_paddle.Down,"Down")
 
 #Creating a l_paddle object from Paddle class
-l_paddle = Paddle((-350,0))
+l_paddle = Paddle((-370,0))
 #Adding eventlisteners to l_paddle
 screen.onkey(l_paddle.Up,"w")
 screen.onkey(l_paddle.Down,"s")
 
 #Creating a ball object using Ball class
 ball = Ball()
+scoreboard = Scoreboard()
 
 #There has to some loop because we need to update the screen every certain second in way 0.02 seconds.
 game_is_on = True
 while game_is_on:
-  time.sleep(0.1) #loop wait for 20 milliseconds
+  time.sleep(ball.move_speed) #loop wait for 20 milliseconds
   screen.update() #To untrace the screen. Back to visible.
   ball.move()
   if ball.ycor() > 280 or ball.ycor() < -280:
-    ball.bounce()
+    ball.bounce_y()
+    #Detect the collisions with paddles
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    ball.bounce_x()
+  #Detect when the paddle misses the ball.
+  #missing the r_paddle
+  if ball.xcor() > 380 :
+    ball.reset_position()
+    scoreboard.l_point()
+  #missing the l_paddle
+  if ball.xcor() < -400:
+    ball.reset_position()
+    scoreboard.r_point()
 
-
-
-
-
-
+    
 screen.exitonclick()
